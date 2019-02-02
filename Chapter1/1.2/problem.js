027166af3f5c5a63164d768c7e014a6e34d180bc
Jeremy Robertson <jrobertson.88@gmail.com>
2019-02-01 22:17:19 -0600

    finished first chapter

--- Chapter1/1.2/problem.js
+++ Chapter1/1.2/problem.js
@@ -2,6 +2,9 @@ var str1 = "This is a list of items: cherries, limes, oranges, apples."
 var start = str1.indexOf(':');
 var end = str1.indexOf('.', start+1);
 
-var listStr = str1.substring(start+1, end);
-var fruits=listStr.split(',');
+var fruits = str1.substring(start+1, end).split(",");
 console.log(fruits);
+
+fruits.forEach(function(elmnt,indx,arry){
+               arry[indx] = elmnt.trim();
+});
